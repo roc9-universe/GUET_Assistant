5bdf9a69b680ac2c2b9c7543dac9a247043e5e3f
lxn <2718495959@qq.com>
2024-10-21 17:21:44 +0800

    init

---
+++ pages/message/message.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		
+		onLoad() {
+			uni.setTabBarBadge({
+			  index: 2,
+			  text: '99'//未读消息数
+			});
+		},
+		
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>

0a4e14345209b3d5a00f86bf2da30d83a5a9eb00
Eorson <Eorson@foxmail.com>
2024-11-01 15:03:16 +0800

    ✨feat(关于我们):添加了关于我们界面
    -添加了关于我们界面，但是没想好写什么进去
    -补充了一点找回密码界面的样式，但是还不知道后端选什么方法找回

---
+++ pages/about/about.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="aboutPage">
+		<view class="main">
+			<view class="menu-item">
+				<text>不知道</text>
+			</view>
+			<view class="menu-item">
+				<text>写什么</text>
+			</view>
+			<view class="menu-item">
+				<text>先这样</text>
+			</view>
+			<view class="menu-item">
+				<text>放着吧</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	.aboutPage{
+		display: flex;
+		flex-direction: column;
+		width: 100vw;
+		height: 100vh;
+		background-color: $bg-color-grey;
+	}
+	
+	.main {
+		padding: 10rpx 20rpx 20rpx 20rpx;
+		margin-top: 50rpx;
+		margin-bottom: 20rpx;
+		background-color: white;
+	
+		.menu-item {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			padding: 20rpx;
+			border-bottom: 1px solid $border-color;
+		}
+	}
+</style>

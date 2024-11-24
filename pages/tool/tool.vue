e6ca4c570a354f6dd221bcbea3c27c74a686e0a1
Eorson <Eorson@foxmail.com>
2024-11-24 17:13:20 +0800

    🌈 style(常用工具): 修改了颜色搭配
    -修改绿色背景为蓝色

--- pages/tool/tool.vue
+++ pages/tool/tool.vue
@@ -12,8 +12,26 @@
 			}
 		},
 		methods: {
-			
-		}
+
+<style scoped lang="scss">
+// .box {
+// 	padding: $page-padding;
+// }
+
+.tool-box {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 20rpx;
+
+	.tool-text {
+		width: 65%;
+		height: 100rpx;
+		text-align: center;
+		line-height: 100rpx;
+		font-weight: bold;
+		margin: 20rpx;
+		color: $text-font-color-1;
 	}
 </script>
 
